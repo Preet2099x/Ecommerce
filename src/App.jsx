3f7888de485a036a9f8b97354eb40432c5b44dcd
Preet2099x <preet2099x@gmail.com>
2024-07-01 03:20:31 +0530

    added context folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import {
   BrowserRouter as Router,
   Route,
@@ -11,18 +10,22 @@ import Orders from './pages/orders/Orders'
 import Cart from './pages/cart/Cart'
 import Dashboard from './pages/admin/dashboard/Dashborad'
 import NoPage from './pages/nopage/NoPage'
+import MyState from './context/data/MyState';
+
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/orders' element={<Orders/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/*' element={<NoPage/>}/>
-      </Routes>
-    </Router>
+    <MyState>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/orders' element={<Orders/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/*' element={<NoPage/>}/>
+        </Routes>
+      </Router>
+    </MyState>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Layout from '../../components/layout/Layout'
+import MyContext from '../../context/data/MyContext'
+
+
 
 function Home() {
+
+  const context = useContext(MyContext)
+  console.log(context)
+
+
   return (
-    <Layout>Home</Layout>
+    <Layout>
+      Home
+    </Layout>
   )
 }
 
 export default Home
+
+
+
+
+
+
+  // Destructure
+  // const {name,order} = context
+  // console.log(name)
+  // console.log(order)
+
+
+  // return (
+  //   <Layout>
+  //     <h1>Name: {name}</h1>
+  //     <h1>Order: {order}</h1>
+  //   </Layout>
+  // )
